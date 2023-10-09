78819ced736d210d7460405514d5882ba0b7c726
Amit00199 <Amit.123854@stu.upes.ac.in>
2023-10-09 09:12:44 +0530

    Hollow Diamond Pattern

---
+++ Patterns/hollow_diamond.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter number of rows: ";
+    int rows;
+    cin >> rows;
+
+    for (int i = 0; i < 2 * rows - 1; i++) {
+
+        int comp;
+        if (i < rows) {
+            comp = 2 * (rows - i) - 1;
+        }
+        else {
+            comp = 2 * (i - rows + 1) + 1;
+        }
+
+        for (int j = 0; j < comp; j++) {
+            cout << " ";
+        }
+  
+        for (int k = 0; k < 2 * rows - comp; k++) {
+            if (k == 0 || k == 2 * rows - comp - 1) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
