5d8ffc08605be12f8eaea01eb84ae73c8b64de3d
Amit00199 <Amit.123854@stu.upes.ac.in>
2023-11-10 11:53:03 +0530

    Added Code

---
+++ Random/ascii_value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "To get the ASCII value for a given character\n";
+    char input;
+    cout<<"Enter any Character: ";
+    cin >> input;
+    cout << "ASCII value of the character is: ";
+    printf("%d", input);
+
+    return 0;
+}
