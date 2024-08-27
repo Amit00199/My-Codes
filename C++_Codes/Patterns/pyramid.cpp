8dec62bd953f88c0b6e1f650252f7e3062f6e704
Amit00199 <Amit.123854@stu.upes.ac.in>
2024-08-27 10:06:59 +0530

    Updated Code

--- C++_Codes/Patterns/pyramid.cpp
+++ C++_Codes/Patterns/pyramid.cpp
@@ -2,17 +2,21 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     cout << "Enter number of rows: ";
     int rows;
     cin >> rows;
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < 2 * (rows - i) - 1; j++) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < 2 * (rows - i) - 1; j++)
+        {
             cout << " ";
         }
 
-        for (int k = 0; k < 2 * i + 1; k++) {
+        for (int k = 0; k < 2 * i + 1; k++)
+        {
             cout << "* ";
         }
         cout << endl;
